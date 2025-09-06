5a264167c860dc6842050515e04c95761899516f
Declan <113652393+Decnj@users.noreply.github.com>
2025-09-07 00:27:20 +0100

    Update user_data.sh

--- task1/user_data.sh
+++ task1/user_data.sh
@@ -1,12 +1,25 @@
 # Start up script that automatically configures and launches our Apache web server
 
 #!/bin/bash
+
+# Update packages
 sudo dnf update -y
+
+# Install Apache web server
 sudo dnf install -y httpd
-sudo cd /var/www/html/
-sudo chmod 644 /var/www/html/
-sudo touch index.html
-sudo chmod 644 /var/www/html/index.html
-sudo echo "<h1>Welcome to my first project at Brainwave Matrix Solutions</h1>" > /var/www/html/index.html
+
+# Start and enable Apache server on instance launch
 sudo systemctl start httpd
 sudo systemctl enable httpd
+
+# Add a simple HTML homepage
+sudo echo "<h1>Welcome to Brainwave Matrix Solutions</h1>
+<h2>Brainwave Matrix Solutions</h2>
+<p>At Brainwave Matrix Solutions, we specialize in transforming 
+digital dreams into reality. We are a dynamic and innovative 
+technology company committed to transforming businesses through 
+cutting-edge IT solutions. Our expertise spans a wide range of 
+services designed to meet the diverse needs of modern enterprises. 
+our mission is to empower businesses by providing robust, scalable, 
+and innovative IT solutions that drive growth and efficiency.</p>" 
+> /var/www/html/index.html
